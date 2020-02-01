e7f83f6f86d62170b3a1ebd27996dd956afe594c
huangkai <q821424508@163.com>
2020-02-01 21:59:01 +0800

    提交

---
+++ spring-webmvc-demo/src/main/java/com/hung/spring/webmvc/demo/MyWebApplicationInitializer.java
@@ -0,0 +1,21 @@
+package com.hung.spring.webmvc.demo;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext container) {
+        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+        appContext.setConfigLocation("/WEB-INF/spring/dispatcher-config.xml");
+
+        ServletRegistration.Dynamic registration = container.addServlet("dispatcher", new DispatcherServlet(appContext));
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
